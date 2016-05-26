ede5dd104ca14434ba63ddeb8d7a8c00bc8b1424
Noel Worden <noelworden@gmail.com>
2016-05-25 22:26:43 -0600

    included tests for empty username and password

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   let(:user){create(:user)}
+  let(:fail_user1) {User.create!(email: "", password: "helloworld", password_confirmation: "helloworld")}
+  let(:fail_user2) {User.create!(email: "example@example.com", password: "", password_confirmation: "")}
 
   describe "attributes" do
     it "should have email" do
@@ -10,6 +12,20 @@ RSpec.describe User, type: :model do
 
     it "should have password" do
       expect(user).to have_attributes(password: user.password)
+    end
+  end
+
+  describe "confirm improper input" do
+    it "should fail the user email" do
+      user.email = nil
+      expect(user).to_not be_valid
+    end
+  end
+
+  describe "testing confirmation" do
+    it "should pass accept the confirmation element" do
+      user.password = nil
+      expect(user).to_not be_valid
     end
   end
 end
