87b8141fff91b0ee56cd3901f78be8f93e586800
Noel Worden <noelworden@gmail.com>
2016-05-28 13:55:15 -0600

    very basic, but directed user to show page after login

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  get 'users/show'
+
   devise_for :users, controllers: {sessions: 'users/sessions'}
   get 'welcome/index'
 
   get 'welcome/about'
 
+  authenticated :user do
+    root to: 'users#show', as: :authenticated_root
+  end
   root 'welcome#index'
 end
