ed2f7c78d513971f7bc3ac54f0b8fec4ee3113dc
Noel Worden <noelworden@gmail.com>
2016-05-28 15:17:08 -0600

    added the _form and _item pages to item view. added the if/else save statement to create method

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,5 +2,12 @@ class ItemsController < ApplicationController
   def create
     @item = Item.new
     @item.user = current_user
+
+    if @item.save
+      flash[:notice] = "Item was saved"
+      redirect_to [@user.show]
+    else
+      flash.now[:alert] = "There was an error"
+    end
   end
 end
