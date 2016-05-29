3b298d461c3a722227da8174ea0e54039e5bd090
Noel Worden <noelworden@gmail.com>
2016-05-28 23:35:09 -0600

    fixed the testing issue, needed to sign in the user and add the 'created_at' inside the factory. now a problem with the create action

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -3,10 +3,15 @@ require 'rails_helper'
 RSpec.describe ItemsController, type: :controller do
 let(:my_user){create(:user)}
 
-  describe "POST #create" do
-    it "increases the number of items by one" do
-      expect {post :create, user_id: my_user.id, item:{name: "test item"}}.to change(Item,:count).by(1)
+  context "signed-in example_user doing CD on a comment they own" do
+    before do
+      sign_in my_user
+    end
+
+    describe "POST create" do
+      it "increase the number of items by 1" do
+        expect {post :create, user_id: my_user.id, item: {name:"This is a test"} }.to change(Item, :count).by(1)
+      end
     end
   end
-
 end
