25083a2d5fecba37e68c467509505b6ed700cb3d
Noel Worden <noelworden@gmail.com>
2016-06-04 18:03:33 -0600

    closes #39. added method to item model to calculate time left

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,13 @@
 class Item < ActiveRecord::Base
+  require 'action_view'
+  include ActionView::Helpers::DateHelper
+
   belongs_to :user
 
   default_scope { order('created_at ASC')}
+
+  def expires_at
+    (created_at + 7.days)
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,39 +1,39 @@
-# 5.times do
-#   User.create!(
-#     email: Faker::Internet.email,
-#     password: RandomData.random_word,
-#     )
-# end
-# users = User.all
+5.times do
+  User.create!(
+    email: Faker::Internet.email,
+    password: RandomData.random_word,
+    )
+end
+users = User.all
 
-# default1 = User.create!(
-#   email: "x@toboom.com",
-#   password: "helloworld"
-#   )
+default1 = User.create!(
+  email: "x@toboom.com",
+  password: "helloworld"
+  )
 
-# defaul2 = User.create!(
-#   email: "x@gmail.com",
-#   password: "helloworld"
-#   )
+defaul2 = User.create!(
+  email: "x@gmail.com",
+  password: "helloworld"
+  )
 
-# 80.times do
-#   Item.create!(
-#     name: Faker::Hipster.sentence(4),
-#     user_id: rand(1..7)
-#     )
-# end
-# items = Item.all
-
-##### Used for an individual seed #######
-30.times do
+80.times do
   Item.create!(
     name: Faker::Hipster.sentence(4),
-    user_id: 6,
-    created_at: "2016-05-28 18:36:42"
+    user_id: rand(1..7)
     )
 end
 items = Item.all
 
-puts "Seed finished"
-puts "#{User.count} users created"
-puts "#{Item.count} itmes created"
+##### Used for an individual seed #######
+# 30.times do
+#   Item.create!(
+#     name: Faker::Hipster.sentence(4),
+#     user_id: 6,
+#     created_at: "2016-05-28 18:36:42"
+#     )
+# end
+# items = Item.all
+
+# puts "Seed finished"
+# puts "#{User.count} users created"
+# puts "#{Item.count} itmes created"
